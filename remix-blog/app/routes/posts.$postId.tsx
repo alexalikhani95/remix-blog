15e1207ee840fc4fec23ccd85335ae4c401fbffd
Alex <alikhanialex@gmail.com>
2024-01-29 15:50:55 +0000

    Minor styling updates

--- remix-blog/app/routes/posts.$postId.tsx
+++ remix-blog/app/routes/posts.$postId.tsx
@@ -3,6 +3,7 @@ import client from "../sanity/client";
 
 import { useLoaderData } from "@remix-run/react";
 import type { LoaderFunctionArgs } from "@remix-run/node";
+import { Link } from "@remix-run/react";
 
 export async function loader({ params }: LoaderFunctionArgs) {
   const postId = params.postId;
@@ -17,10 +18,13 @@ const Post = () => {
   const post = data[0];
 
   return (
-    <div>
-      <h2>{post.title}</h2>
-      {post.author && <p>Author{post.author}</p>}
-      {post.body && <p>{post.body[0].children[0].text}</p>}
+    <div className="container mx-auto px-4 py-8">
+        <Link to="/posts" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Back to posts</Link>
+      <div className="max-w-lg mx-auto bg-white rounded-lg shadow-lg p-6 mt-5">
+        <h2 className="text-2xl font-semibold text-gray-800">{post.title}</h2>
+        {post.author && <p className="mt-2 text-gray-600">Author: {post.author}</p>}
+        {post.body && <p className="mt-4 text-gray-700">{post.body[0].children[0].text}</p>}
+      </div>
     </div>
   );
 };
--- remix-blog/app/routes/posts._index.tsx
+++ remix-blog/app/routes/posts._index.tsx
@@ -11,21 +11,21 @@ const Posts = () => {
   const posts = useLoaderData<typeof loader>();
 
   return (
-    <>
-      <div className="flex flex-col items-center">
-        <h2 className="mb-5">Posts</h2>
+    <div className="container mx-auto px-4 py-8">
+      <div className="max-w-lg mx-auto">
+        <h2 className="text-2xl font-semibold mb-5">Blog Posts</h2>
         {posts.length &&
           posts.map((post: PostType) => {
             return (
-                <Link to={`/posts/${post._id}`} key={post._id}>
-              <div key={post._id}>
-                <h3>{post.title}</h3>
-              </div>
+              <Link to={`/posts/${post._id}`} key={post._id}>
+                <div className="bg-white rounded-lg shadow-md p-4 mb-4 cursor-pointer transition duration-300 hover:shadow-lg">
+                  <h3 className="text-lg font-semibold text-gray-800">{post.title}</h3>
+                </div>
               </Link>
             );
           })}
       </div>
-    </>
+    </div>
   );
 };
 
